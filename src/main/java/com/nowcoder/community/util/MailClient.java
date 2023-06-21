7462a138de5f113e56415665e4c325e3dd5c8eeb
FuCheng <wz840332892@gmail.com>
2023-06-21 19:54:02 +0800

    完成了邮箱发送

---
+++ src/main/java/com/nowcoder/community/util/MailClient.java
@@ -0,0 +1,41 @@
+package com.nowcoder.community.util;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MailClient {
+    private static final Logger logger = LoggerFactory.getLogger(MailClient.class);
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void sendMail(String to, String subject, String content) {
+
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content,true); //TIP 加了true后，你把html转成字符串传进来它也能识别
+            mailSender.send(helper.getMimeMessage()); //TIP我说怎么收不到，原来是忘记发送了
+        } catch (MessagingException e) {
+            logger.error("发送邮件失败：" + e.getMessage());
+        }
+
+    }
+
+
+}
